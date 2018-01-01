c34cfd9d4d1fd334c29ef4caa517c6807c68ee5b
Sergio Diosdado <sgdc_98@hotmail.com>
2017-12-31 19:06:54 -0600

    AsÃ­ nomÃs quedÃ³#

---
+++ public/javascripts/script.js
@@ -0,0 +1,6 @@
+function openNav(){
+    document.getElementById("Navbar").style.transform = "translateX(0)";
+}
+function closeNav(){
+    document.getElementById("Navbar").style.transform = "translateX(100%)";
+}
