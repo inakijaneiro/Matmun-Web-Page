8896e81d1a1f83f69bef9c84042ffc7073dcfb9d
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2018-02-11 21:27:22 -0600

    Edited meta data for performance boost and script for the automatic reload of scheduled content

--- public/javascripts/horario.js
+++ public/javascripts/horario.js
@@ -1,9 +1,9 @@
 var date = new Date().getTime();
-var date2 = new Date("Sat Feb 11 2018 18:15:20 GMT-0600 (CST)").getTime();
+var date2 = new Date("Sat Mar 8 2018 23:59:59 GMT-0600 (CST)").getTime();
 function creaHora(){
 	date = new Date().getTime();
-	console.log(date);
-	console.log(date2);
+	//console.log(date);
+	//console.log(date2);
 	if (date2 < date){
 		clearInterval(intervalo);
 		document.querySelectorAll('.Horario')[0].classList.toggle("Hoy");
